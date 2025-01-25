74dae391d8059f030c7062f8fd5fdaf3704cfd12
Randomness <riftgames1@outlook.com>
2025-01-24 22:49:27 -0500

    The final push (hopefully)

---
+++ public/js/background.js
@@ -0,0 +1,11 @@
+function changeBackground() {
+    const user = JSON.parse(localStorage.getItem('user'));
+    const schoolName = user.school === 'hse' ? 'HSE' : 'Fishers';
+    if(schoolName == "Fishers"){
+
+        document.body.style.backgroundImage = "url('img/wwww.jpg')";
+    }else {
+
+        document.body.style.backgroundImage = "url('img/wwww.jpg')";
+    }
+}
